290b795294709bff8d3eda2683654e9875f1e73f
ArnoldSupriyadi <arnoldsupriyadi@gmail.com>
2022-04-13 22:47:54 +0700

    menambahkan asset

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,15 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/RegisterView.vue')
     },
+    {
+      // path: '/details/id',
+      path: '/product',
+      name: 'product',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ProductView.vue')
+    },
   ]
 })
 
